ed93cc1706a4e17e5c5392e805bd28d26b2b226a
Vladislav-Ovsyannikov <vladikslav25@gmail.com>
2024-03-14 16:23:32 +0300

    Create DB

---
+++ db/migrations/20240314122843-create-questions.js
@@ -0,0 +1,40 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Questions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      question: {
+        type: Sequelize.STRING
+      },
+      topic_id: {
+        type: Sequelize.INTEGER,
+        references:{
+          model:"Topics",
+          key:"id"
+        },
+        onUpdate:'cascade',
+        onDelete:'cascade'
+      },
+      answer: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: true,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: true,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Questions');
+  }
+};
