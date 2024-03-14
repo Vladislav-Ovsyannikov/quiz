4bce5492aad0883cd822c860c45458c5caa9da31
LebedevD174 <Lebedevdima174@yandex.ru>
2024-03-14 16:30:37 +0300

    create MainPage and main.route

---
+++ components/pages/MainPage.jsx
@@ -0,0 +1,11 @@
+const React = require('react');
+
+module.exports = function MainPage({ title }) {
+  return (
+    <>
+      <div><img src="" alt="Очень милая картинка" /></div>
+      <div>Приветсвуем вас на квизе от "Рабочее название команды"</div>
+      <button>Начать квиз</button>
+    </>
+  );
+};
