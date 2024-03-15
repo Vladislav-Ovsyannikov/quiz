0c16d65ea15e04781c1c0e4a99475dbfa13f18fe
MaryFBakeer <mariabefrank@gmail.com>
2024-03-15 10:38:29 +0300

    release 2

--- routes/index.router.js
+++ routes/index.router.js
@@ -1,15 +1,21 @@
 const router = require('express').Router();
 
+//views
 const authRouter = require('./views/auth.routes');
 const topicViewRouter = require('./views/topic.router.views');
-
 const mainRoute = require('./views/main.route');
 const scoresRoute = require('./views/scores.router');
 
+//api
+const questionRouter = require('./api/question.api.router');
+
+//router views
 router.use('/', mainRoute);
 router.use('/topics', topicViewRouter);
 router.use('/auth', authRouter);
-
 router.use('/scores-table', scoresRoute);
+
+//router api
+router.use('/api/question', questionRouter);
 
 module.exports = router;
